7eb277c5a06ca16d0770b284e9bb3ba029b9902c
Александр <59563952+MIMBOL228@users.noreply.github.com>
2022-05-03 23:49:57 +0500

    Синхронизация с github.com/mimbol228/mysqlo

--- mysql.php
+++ mysql.php
@@ -1,50 +1,50 @@
 <?php
-
-class mysqlo
-{
-    private $connection;
-
-    function __construct($multi, string $l = "", string $p = "", string $db = "")
-    {
-        if ((!is_array($multi) && ($multi == "" || $l == "" || $db == "")) || (is_array($multi) && (($l != "" || $db != "") || count($multi) != 4))) {
-            $t = debug_backtrace()[0];
-            die("<br>Ошибка синтаксиса! Проверьте код в файле: " . $t["file"] . " , на строке " . $t["line"] . "<br>");
-        }
-        if (is_array($multi)) {
-            $ip = $multi['ip'];
-            $l = $multi['user'];
-            $p = $multi['password'];
-            $db = $multi['dbname'];
-        } else {
-            $ip = $multi;
-        }
+class mysqlo{
+    public $connect; // Переменная объекта соединения
+    function __construct(array $data){ // Конструктор класса
         try {
-            $this->connect = new PDO("mysql:dbname=$db;host=$ip;charset=utf8;", $l, $p);
-        } catch (PDOException $exception) {
-            error_verify('adm');
+            $this->connect = new PDO(
+                "mysql:dbname={$data['dbnm']};host={$data['addr']};charset=utf8;" ,
+                $data['user'],
+                $data['pass']
+            ); // Создание подключения
+            $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) { // При ошибке
+            trigger_error('Ошибка подключения Базы Данных: ' . $exception->getMessage(), E_USER_ERROR); // Пишем её
         }
     }
-
-    function startsWith($string, $startString)
-    {
-        $len = strlen($startString);
-        return (substr($string, 0, $len) === $startString);
-    }
-
-    function query(string $sql, array $parameters)
-    {
-        $statement = $this->connect->prepare($sql);
-        $newParameters = [];
-        foreach ($parameters as $key => $value) {
-            if (!$this->startsWith($key, ':')) {
-                $newParameters[':' . $key] = $value;
-            } else {
-                $newParameters[$key] = $value;
+    function query(string $sql, array $parameters, bool $all = false){ // Функция выполнения запроса к MySql
+        try {
+            $statement = $this->connect->prepare($sql); // Подготавлеваем запрос
+            $newParameters = array_combine(
+                array_map(function($k){ return ':'.$k; }, array_keys($parameters))
+                , $parameters
+            );
+            $statement->execute($newParameters); // Выполняем запрос
+            if($all){ // Если передано, что передавать все полученные данные, то
+                return $statement->fetchAll(PDO::FETCH_ASSOC); // Передаём все даннные
+            }else{ // Иначе
+                return $statement->fetch(PDO::FETCH_ASSOC); // Передаём первые попавшиеся
             }
+        } catch (Exception $exception) { // При ошибке
+            trigger_error( 'Ошибка запроса: ' . $exception->getMessage(), E_USER_ERROR); // Пишем её
         }
-        $statement->execute($newParameters);
-        if (false !== stripos($sql, 'SELECT'))
-            return $statement->fetch(PDO::FETCH_ASSOC);
-        return [];
+    }
+    function transaction(array $query_list){ // Функция удобной транзакции
+        try {
+            $this->connect->beginTransaction(); // Объявляем начало транзакции
+            foreach($query_list as $sql => $parameters){ // Проходимся по каждому запросу
+                $statement = $this->connect->prepare($sql); // Подготавлеваем запрос
+                $newParameters = array_combine(
+                    array_map(function($k){ return ':'.$k; }, array_keys($parameters))
+                    , $parameters
+                );
+                $statement->execute($newParameters); //Выполняем запрос
+            }
+            $this->connect->commit(); // Сохраняем изменения
+        } catch (Exception $exception) { // Если что-то пошло не так
+            $this->connect->rollBack(); // Откатываем (отменяем) изменения
+            trigger_error('Ошибка транзакции: ' . $exception->getMessage(), E_USER_ERROR); // И выводим ошибку
+        }
     }
 }
